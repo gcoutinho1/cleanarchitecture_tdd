f8282648e37752ee17194873533c9e62a8c1838b
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2022-06-12 15:30:16 -0300

    Added Data Layer Overview & Models for NumberTrivia and others unit tests

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 
 ---
 
+if you are interested in watching this course, please check this [playlist](https://www.youtube.com/watch?v=KjE2IDphA_U&list=PLB6lc7nQ1n4iYGE_khpXRdJkJEp9WOech&index=1&ab_channel=ResoCoder).  
+
+as the course was recorded in 2019 and at that time dart did not have **null-safety** some things you will need to do in a different way if you are using the **Dart** version ```>=2.12``` ```&&``` **Flutter** ```>= 2.0```.
+
+For example when making class mocks for **unit tests** you will need to read this documentation here: [Mockito null-safety](https://github.com/dart-lang/mockito/blob/master/NULL_SAFETY_README.md)   
+See also: [Dart null-safety](https://dart.dev/null-safety)
+
+---
+
 <p align="center">
   <img src="https://github.com/gcoutinho1/cleanarchitecture_tdd/blob/main/imgs/cleanarchitecture.jpg">
 </p>
---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:cleanrchitecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:cleanrchitecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test text');
+
+  test(
+    'Should be a subclass of NumberTrivia entity',
+    () async {
+      expect(tNumberTriviaModel, isA<NumberTrivia>());
+    },
+  );
+  group('fromJson', () {
+    test('Should return a valid model when the JSON number is an integer',
+        () async {
+      //arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+      //act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      //assert
+      expect(result, tNumberTriviaModel);
+    });
+
+    test(
+        'Should return a valid model when the JSON number is regarded as a double',
+        () async {
+      //arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('trivia_double.json'));
+      //act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      //assert
+      expect(result, tNumberTriviaModel);
+    });
+  });
+
+  group('toJson', () {
+    test('Should return a JSON map containing the proper data', () async {
+      // act
+      final result = tNumberTriviaModel.toJson();
+      // assert
+      final expectedMap = {
+        "text": "Test text",
+        "number": 1
+      };
+      expect(
+        result, expectedMap
+      );
+    });
+  });
+}
