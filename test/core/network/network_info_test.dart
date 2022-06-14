1392ddf17422d3a918bce466fcec53eb317de822
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2022-06-14 16:12:55 -0300

    refactor test of group[isConnected]

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -19,13 +19,14 @@ void main() {
     test('Should forward the call to DataConnectionChecker.hasConnection',
         () async {
       // arrange
+      final tHasConnectionFuture = Future.value(true);
       when(() => mockDataConnectionChecker.hasConnection)
-          .thenAnswer((_) async => true);
+          .thenAnswer((_) => tHasConnectionFuture);
       // act
-      final result = await networkInfoImpl.isConnected;
+      final result = networkInfoImpl.isConnected;
       // assert
       verify(() => mockDataConnectionChecker.hasConnection);
-      expect(result, true);
+      expect(result, tHasConnectionFuture);
     });
   });
 }
